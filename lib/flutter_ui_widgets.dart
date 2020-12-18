93b74fad8c3d34ebe53ccea8b8f8ce461ac95673
Fabián Karaben <fabiankaraben@gmail.com>
2020-12-17 21:37:11 -0300

    to i7

--- lib/flutter_ui_widgets.dart
+++ lib/flutter_ui_widgets.dart
@@ -1,5 +1,7 @@
 library flutter_ui_widgets;
 
+export 'src/shared/option_horizontal_container.dart';
+export 'src/shared/option_vertical_container.dart';
 export 'src/color_option.dart';
 export 'src/dropdown_option.dart';
 export 'src/number_option.dart';
